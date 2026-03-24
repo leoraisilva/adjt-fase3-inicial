958ece9440d5203495203570ed2f456a6446158c
leoraisilva <lraimundo409@gmail.com>
2026-03-24 00:01:44 -0300

    finalizando Exception

--- hospital/historico/src/main/java/br/com/fiap/hospital/historico/infra/adapter/outbound/exception/ResponseException.java
+++
@@ -1,4 +0,0 @@
-package br.com.fiap.hospital.historico.infra.adapter.outbound.exception;
-
-public record ResponseException(String type, String title, int status, String detail, String instance) {
-}
